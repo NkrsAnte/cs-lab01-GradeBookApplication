c0c5ea93651e79c8c6f6a1f9c35d86336333a8f3
NkrsAnte <79981829+NkrsAnte@users.noreply.github.com>
2022-04-03 18:56:11 +0200

    11 done

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -34,6 +34,15 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';
         }
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading reqires at least 5 students");
+                return;
+            }
+            base.CalculateStatistics();
+        }
 
     }
 }
